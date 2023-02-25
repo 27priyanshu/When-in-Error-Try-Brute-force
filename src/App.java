c6700fbdd111dcd1540969b229e9e350ce72a298
Priyanshu <2003priyanshusingh@gmail.com>
2023-02-25 21:03:45 +0530

    basic prog

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //println will print in next line.
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
---
+++ src/add2num.java
@@ -0,0 +1,12 @@
+public class add2num {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int a = i + j;
+        System.out.print(a);
+
+        i = 2;
+
+    }
+}
