a6fd37620447719e97f1b34ba12d45f758eefc20
Priyanshu <2003priyanshusingh@gmail.com>
2023-03-09 09:56:57 +0530

    changes in variables

--- src/simpleintrestcal.java
+++ src/simpleintrestcal.java
@@ -2,9 +2,9 @@ public class simpleintrestcal {
 
     public static void main(String[] args) {
         int p = 1000;
-        int r = 10;
+        int a = 10;
         int t = 5;
-        int si = (p * r * t) / 100;
+        int si = (p * a * t) / 100;
 
         System.out.println(si);
 
